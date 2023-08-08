a47ea9c495515d326ba8b824092a0ad7a882042b
Maria De Fátima <133269366+Maria621@users.noreply.github.com>
2023-08-08 07:57:36 -0300

    Atualizando o nav bar e as rotas

--- src/App.tsx
+++ src/App.tsx
@@ -5,35 +5,40 @@ import Navbar from './components/navBar/NavBar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './paginas/login/Login';
-
+import Cadastro from './paginas/cadastro/Cadastro';
 import Home from './paginas/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
 import ListaTemas from './components/temas/listaTemas/ListaTemas';
-
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import Sobre from './paginas/sobre/Sobre'
 
 
 function App() {
   return (
-    <>
-    <AuthProvider>
-        <BrowserRouter>
-          <Navbar />
-          <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/login" element={<Login />} />
-        
-              <Route path="/home" element={<Home />} />
-              <Route path="/temas" element={<ListaTemas />} />
-
-              <Route path="/cadastro" element={<Cadastro />} />
-              <Route path="/home" element={<Home />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-        </AuthProvider>
-    </>
+   
+      <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <div className='min-h-[80vh]'>
+          {/* Renderize o componente Home fora das rotas */}
+          
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/temas" element={<ListaTemas />} />
+            <Route path="/cadastroTema" element={<FormularioTema />} />
+            <Route path="/editarTema/:id" element={<FormularioTema />} />
+            <Route path="/deletarTema/:id" element={<DeletarTema />} />
+            
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 export default App;
--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -2,38 +2,58 @@ import React, { useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../contexts/AuthContext'
 
-
-
 function Navbar() {
   let navigate = useNavigate()
-
   const { usuario, handleLogout } = useContext(AuthContext)
 
   function logout() {
-      handleLogout()
-      alert('Usuário deslogado com sucesso')
-      navigate('/login')
+    handleLogout()
+    alert('Usuário deslogado com sucesso')
+    navigate('/login')
   }
 
   let navbarComponent
-  
+
+  if (usuario.token !== "") {
+    navbarComponent = (
+      <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+        <div className="container flex justify-between text-lg">
+          <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
+
+          <div className='flex gap-4'>
+            <Link to='/postagens' className='hover:underline'>Home</Link>
+            <Link to='/postagens' className='hover:underline'>Agendamento</Link>
+            <Link to='/sobre' className='hover:underline'>Sobre</Link>
+            <Link to='/postagens' className='hover:underline'>Postagens</Link>
+            <Link to='/temas' className='hover:underline'>Temas</Link>
+            <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
+            <Link to='/perfil' className='hover:underline'>Perfil</Link>
+            <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+          </div>
+        </div>
+      </div>
+    )
+  } else {
+    // Adicione o return aqui para renderizar os links quando o usuário não está logado
+    navbarComponent = (
+      <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+        <div className="container flex justify-between text-lg">
+          <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
+
+          <div className='flex gap-4'>
+            <Link to='/home' className='hover:underline'>Home</Link>
+            <Link to='/sobre' className='hover:underline'>Sobre</Link>
+            <Link to='/postagens' className='hover:underline'>Postagens</Link>
+            <Link to='/login' className='hover:underline'>Login</Link>
+          </div>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <>
-     <div className='w-full bg-purple-100 text-white flex justify-center py-4'>
-          <div className="container flex justify-between text-lg">
-          <Link to='/home' className='text-2xl font-bold uppercase'>Minas na Area</Link>
-
-            <div className='flex gap-4'>
-            <div className='hover:underline'>Postagens</div>
-            <Link to='/temas' className='hover:underline'>Temas</Link>
-            <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
-              <div className='hover:underline'>Perfil</div>
-              <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
-             
-            </div>
-          </div>
-        </div>
+      {navbarComponent}
     </>
   )
 }
