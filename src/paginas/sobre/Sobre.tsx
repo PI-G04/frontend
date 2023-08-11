1d8a3c36444a25d169e5b9b717ea78278396b407
Maria De Fátima <133269366+Maria621@users.noreply.github.com>
2023-08-11 15:57:54 -0300

    23

--- src/paginas/sobre/Sobre.tsx
+++ src/paginas/sobre/Sobre.tsx
@@ -42,7 +42,7 @@ const memberElements = groupMembers.map((member, index) => (
      {member.photo} 
     </div>
     <h3 className="text-lg text-center font-semibold">{member.name}</h3>
-    <div className="Perfil_icons flex m-4 gap-2 space-x-3 ">
+    <div className="Perfil_icons flex m-4 gap-2 space-x-2 ">
       <a href={member.linkedin} target="_blank" rel="noreferrer">
         <img src={Linkedin} alt={`LinkedIn de ${member.name}`} />
       </a>
@@ -56,27 +56,26 @@ const memberElements = groupMembers.map((member, index) => (
 function Sobre() {
     return (
        
-        <div className="bg-purple-400 ...">
-        <div className="bg-purple-500 flex justify-center">
-          <div className='container grid grid-cols-2 text-white'>
-            <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-bold'>Sobre nós!</h2>
-              <p className='text-xl'>Nossa trajetoria ate aqui</p>
+  <div className="">
+    <div className="bg-pink-200 L3 flex justify-center">
+      <div className='bg-pink-300 container grid grid-cols-2 text-white'>
+        <div className="flex flex-col gap-9 items-center justify-center">
+          <h2 className='text-5xl font-bold'>Sobre nós!</h2>
+            <p className='text-xl'>Nossa trajetoria ate aqui</p>
   
-            </div>
-            <div className="flex justify-center ">
-              <img src={sobreLogo} alt="" className='w-2/3' />
+          </div>
+        <div className="flex justify-center ">
+      <img src={sobreLogo} alt="" className='w-2/3' />  
+    </div>
+  </div>
+</div>
         
-              </div>
-            </div>
-        </div>
-     
-
-        <div className="bg-purple-400 ...">
-        <div className=" flex justify-center">
-          <div className='container grid grid-cols-1 text-black'>
-            <div className="flex flex-col gap-4 items-center justify-center p-40 py-4">
-  <h2 className='text-5xl p-20 text-center '>Ola, conheça quem somos!</h2>
+  <div className="bg-pink-200 L">
+    <div className="bg-pink-200 ">
+    <div className=" flex justify-center ">
+      <div className='container grid grid-cols-1 p-20 bg-pink-100 text-purple-400'>
+        <div className="flex flex-col gap-4 items-center justify-center py-5">
+          <h2 className='text-5xl bottom-spacing text-center '>Ola, conheça quem somos!</h2>
             <p className='text-xl text-center '>Num cenário onde a igualdade de gênero era um 
               ideal a ser alcançado, cinco jovens, unidos por uma 
               visão de transformação, decidiram criar um projeto revolucionário de 
@@ -84,7 +83,8 @@ function Sobre() {
               Com o objetivo central de ampliar a presença feminina nas mais variadas
               empresas e setores, esse grupo tinha uma missão clara: 
               aumentar o índice de empregabilidade das mulheres e encorajá-las a 
-              ocupar cargos de liderança e influência.</p>
+              ocupar cargos de liderança e influência.
+            </p>
             
             <p className='text-xl text-center '>A essência do projeto era inspirar e apoiar 
               mulheres a ingressarem em áreas tradicionalmente dominadas por homens. 
@@ -94,9 +94,10 @@ function Sobre() {
               diversificada e talentosa. O projeto não apenas visava aumentar o número
                de mulheres no mercado de trabalho, mas também garantir que elas 
                ocupassem posições de alto calão e contribuíssem significativamente em 
-               diversos setores.</p>
+               diversos setores.
+            </p>
 
-              <p className='text-xl text-center '>Com paixão e comprometimento, o grupo criou 
+            <p className='text-xl text-center '>Com paixão e comprometimento, o grupo criou 
             uma plataforma inovadora que oferecia sessões virtuais com profissionais 
             especializados em empregabilidade. Essas sessões eram direcionadas para 
             ajudar as mulheres a construir currículos impressionantes, aprimorar suas 
@@ -111,13 +112,15 @@ function Sobre() {
                precocemente devido a razões pessoais. Sua partida trouxe uma mistura 
                de tristeza e determinação para os outros membros do grupo. Eles sabiam
                 que a melhor maneira de honrar sua contribuição era continuar com o 
-                projeto e transformar a visão em realidade.</p>
+                projeto e transformar a visão em realidade.
+            </p>
 
-             <p className='text-xl text-center '>Além de suas metas intrínsecas, o projeto 
+            <p className='text-xl text-center '>Além de suas metas intrínsecas, o projeto 
              estava alinhado com os Objetivos de Desenvolvimento Sustentável (ODS), 
              especialmente o ODS 5 - Igualdade de Gênero. A meta 5.B, que promove o 
              empoderamento das mulheres através das tecnologias de informação e 
-             comunicação, ressoava profundamente com a essência do projeto.</p>
+             comunicação, ressoava profundamente com a essência do projeto.
+            </p>
             
             <p className='text-xl text-center'>Assim, impulsionados pela paixão, empatia e visão 
               de futuro, quatro dos jovens continuaram a jornada que começaram. O 
@@ -126,23 +129,24 @@ function Sobre() {
               homenagem ao membro que partiu, uma mulher cujo espírito inspirador 
               permaneceria sempre presente. E assim, eles se comprometeram a transformar 
               vidas, enfrentar desafios e pavimentar o caminho para um futuro mais justo, 
-              igualitário e brilhante para todos.</p>
-              </div>
-              </div>
-              </div>
-              </div>
-      <div className="bg-purple-400 flex justify-center items-center">
-        <div className="text-center p-20 m-3">
-        <h2 className='text-5xl p-12 font-bold'>Nossos Membros</h2>
-          <div className="flex flex-wrap justify-center items-center space-x-8 m-10">
-        
-              {memberElements}
-              </div>
-            </div>
+              igualitário e brilhante para todos.
+            </p>
+          </div>
         </div>
       </div>
-   
+    </div>
+    </div>
+    <div className="bg-pink-200 flex justify-center items-center text-purple-400">
+      <div className="text-center bg-pink-300 L2">
+        <h2 className='text-5xl EL font-bold'>Nossos Membros</h2>
+          <div className="flex flex-wrap justify-center items-center space-x-9 m-10">
+        
+              {memberElements}
 
-    );
+          </div>
+      </div>
+    </div>
+  </div> 
+ );
 }
 export default Sobre;
